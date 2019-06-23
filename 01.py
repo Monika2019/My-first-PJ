0518cccac45e7e0a2ac15a87393534bf3d2ee24a
Monika2019 <52123265+Monika2019@users.noreply.github.com>
2019-06-23 19:10:24 +0800

    mm豆

--- 01.py
+++ 01.py
@@ -1,25 +1,20 @@
 # -*- coding: UTF-8 -*-
 
-# Filename : 02-switching.py
-# author by : （学员ID)
+# Filename : 01-helloworld.py
+# author by : （Monika)
 
 # 目的:
-# 掌握基本的赋值，输入及输出方法
-# 掌握 print 代入模式
+# 掌握如何查看程序运行结果的方式
 
-
-# -------------------------------
-# 练习一
-# 用户输入
-x = input('输入 x 值: ')
-y = input('输入 y 值: ')
-
-# 创建临时变量，并交换
-temp = x
-x = y
-y = temp
-
-print('交换后 x 的值为: ')
-print (x)
-print('交换后 y 的值为: ')
-print (y)
+# 该实例输出 Hello World!
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
